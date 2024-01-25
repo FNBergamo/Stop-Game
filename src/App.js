13659069ed6ec79db4a33f0b5aaee5bb674ffbc2
FNBergamo <felipe.nbergamo@gmail.com>
2024-01-25 15:53:27 -0300

    Add basic letter draw functionality with timer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import dayjs from 'dayjs'
+import './App.css'
 
-function App() {
+const DEFAULT_LETTERS_ARRAY = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'I',
+  'J',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+]
+export default function App() {
+  const initialTime = 120
+  const [elapsedTime, setElapsedTime] = useState(0)
+  const [letters, setLetters] = useState(DEFAULT_LETTERS_ARRAY)
+  const [letter, setLetter] = useState('')
+
+  useEffect(() => {
+    const timerInterval = setInterval(() => {
+      setElapsedTime((prevTime) => (prevTime > 0 ? prevTime - 1 : 0))
+    }, 1000)
+
+    return () => {
+      clearInterval(timerInterval)
+    }
+  }, [])
+
+  const getRandomIndex = () => Math.floor(Math.random() * letters.length)
+
+  const getRandomLetter = () => {
+    const luckyNumber = getRandomIndex()
+    const randomLetter = letters[luckyNumber]
+    setLetters((prevLetters) => prevLetters.filter((l) => l !== randomLetter))
+    setLetter(randomLetter)
+  }
+
+  const resetTimer = () => setElapsedTime(initialTime)
+
+  const newRound = () => {
+    getRandomLetter()
+    resetTimer()
+  }
+
+  const formattedTime = dayjs().startOf('day').second(elapsedTime).format('mm:ss')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <p>{formattedTime}</p>
+      <p>{letter}</p>
+      <button onClick={newRound}>Sortear</button>
     </div>
-  );
+  )
 }
 
-export default App;
